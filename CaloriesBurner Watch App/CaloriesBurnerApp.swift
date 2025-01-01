18c18a25c741621843749364cfbb952550d2501c
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2025-01-01 11:50:43 +0100

    watchOS blank project created

---
+++ CaloriesBurner Watch App/CaloriesBurnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesBurnerApp.swift
+//  CaloriesBurner Watch App
+//
+//  Created by Javier Calatrava on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesBurner_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
